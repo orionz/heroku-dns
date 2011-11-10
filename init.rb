4da7dbef70da2cc129e2ed6ee25dd8889717cb32
Orion Henry <orion@heroku.com>
2011-11-09 16:35:43 -0800

    v1

---
+++ init.rb
@@ -0,0 +1,2 @@
+
+require 'heroku/commands/dns'
---
+++ lib/heroku/commands/dns.rb
@@ -0,0 +1,21 @@
+module Heroku::Command
+  # manage Heroku DNS
+  #
+  class Dns < BaseWithApp
+    # DNS
+    #
+    # herp derp
+    #
+    def index
+      puts "Derp"
+    end
+
+    # dns:derp [FILE]
+    #
+    # Herpa derpa
+    #
+    def dump
+      puts "Herp Derp"
+    end
+  end
+end
