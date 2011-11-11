24513297dcd8b2af9b149c142e4285699e6ec72d
Orion Henry <orion@heroku.com>
2011-11-11 13:50:03 -0800

    x

--- init.rb
+++ init.rb
@@ -1,2 +1,2 @@
-
+require 'json'
 require 'heroku/commands/dns'
--- lib/heroku/commands/dns.rb
+++ lib/heroku/commands/dns.rb
@@ -4,18 +4,106 @@ module Heroku::Command
   class Dns < BaseWithApp
     # dns
     #
-    # herp derp
+    # List all domains under management
     #
     def index
-      puts "Derp"
+      domains = JSON.parse(resource("/dns").get)
+      puts domains.inspect
+      display "You have #{domains.length} domains under management."
+      domains.each do |d|
+        display "  #{d["zone"]["name"]}"
+      end
     end
 
-    # dns:derp [FILE]
+    # dns:describe DOMAIN
     #
-    # Herpa derpa
+    # describe a domain and its current records
     #
-    def dump
-      puts "Herp Derp"
+    def describe
+      domain = args.shift.downcase rescue nil
+      fail("Usage: heroku dns:describe DOMAIN") unless domain
+      result = JSON.parse(resource("/dns/#{domain}").get)
+      display result["zone"]["name"]
+      result["zone"]["records"].each { |r| display sprintf("    %-16s -> %s",r["record"]["domain"], r["record"]["app"]) }
+      display "Nameservers:"
+      result["zone"]["servers"].each { |s| display "    #{s}" }
     end
-  end
+
+    # dns:add DOMAIN
+    #
+    # Add a new domain to be managed
+    #
+    def add
+      domain = args.shift.downcase rescue nil
+      fail("Usage: heroku dns:add DOMAIN") unless domain
+      begin
+        result = JSON.parse(resource("/dns").post :domain => domain)
+        display "Added #{domain}"
+        display "Please update your dns servers to be:"
+        result["servers"].each do |server|
+          puts "  #{server}"
+        end
+      rescue RestClient::Forbidden => e
+        display e.response
+      end
+    end
+
+    # dns:remove DOMAIN
+    #
+    # Remove a domain from management.  Make sure
+    # you have already set up alternate DNS servers
+    # before doing this!
+    #
+    def remove
+      domain = args.shift.downcase rescue nil
+      fail("Usage: heroku dns:add DOMAIN") unless domain
+      result = resource("/dns/#{domain}").delete
+      display "Deleting #{domain}"
+      puts result.inspect
+    end
+
+    # dns:map DOMAIN APP
+    #
+    # Map a domain to an app
+    #
+    def map
+      domain = args.shift.downcase rescue nil
+      app = args.shift.downcase rescue nil
+      fail("Usage: heroku dns:map DOMAIN APP") unless domain and app
+      result = resource("/dns/#{domain}/map/#{app}").put({})
+      display "Mapping #{domain} to #{app}"
+      puts result.inspect
+    end
+
+    # dns:unmap DOMAIN
+    #
+    # Unmap a domain
+    #
+    def unmap
+      domain = args.shift.downcase rescue nil
+      fail("Usage: heroku dns:unmap DOMAIN") unless domain
+      result = resource("/dns/#{domain}/map/").put({})
+      display "Unmapping #{domain}"
+      puts result.inspect
+    end
+
+    private
+
+    def resource(path, options={})
+      uri = "https://dnsx.herokuapp.com"
+      RestClient.proxy = ENV['HTTP_PROXY'] || ENV['http_proxy']
+      puts "RestClient::Resource.new '#{uri}#{path}', #{options.merge(:user => user, :password => password).inspect}"
+      RestClient::Resource.new "#{uri}#{path}", options.merge(:user => user, :password => password)
+    end
+
+    def user
+#      Heroku::Auth.user
+      ""
+    end
+
+    def password
+      Heroku::Auth.password
+    end
+
+   end
 end
